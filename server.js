49120047d58e1019db88bb1c1e3a82b3cbf1923c
Eric P. Katz <ericpkatz@gmail.com>
2017-01-31 20:03:33 -0500

    create express app

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const swig = require('swig');
+swig.setDefaults({ cache: false });
+const sqlite = require('sqlite3');
+
+const db = new sqlite.Database(process.env.DB, (err)=> console.log(err));
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+app.get('/', (req, res, next)=> res.render('index'));
+
+app.post('/', (req, res, next)=> {
+  const sql = req.body.sql;
+  db.all(sql, (err, results)=> {
+    res.render('index', { sql , results: results, error: err });
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
